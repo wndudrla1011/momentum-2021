04942ca6e977e0c482d79a8221f22eab8ff6271e
wndudrla1011 <wndudrla1011@naver.com>
2021-11-01 02:09:57 +0900

    test finished

---
+++ js/music.js
@@ -0,0 +1,8 @@
+const backgroundMusic = document.querySelector(".background-music");
+
+function playMusic() {
+  backgroundMusic.volume = 0.5;
+  backgroundMusic.loop = true;
+}
+
+window.addEventListener("load", playMusic);
