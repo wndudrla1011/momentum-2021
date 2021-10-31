d78da4fb0d017d110d40fb0080defba0e40a70d2
wndudrla1011 <wndudrla1011@naver.com>
2021-11-01 03:09:50 +0900

    image-error-4 updated

--- js/foodImage.js
+++ js/foodImage.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.JPG"];
 const title = [
   "육회비빔밥",
   "돈카츠 정식",
